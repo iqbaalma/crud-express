653995cda03359c634edf562c1d0de8915971d1d
Iqbal Maulana <suraatiqbaal@gmail.com>
2023-10-29 20:41:07 +0700

    update(crud): express project for crud is done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(methodOverride('_method'))
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
 
-const comments = [
+let comments = [
     {
         id: uuidv4(),
         username: "Alexa",
@@ -70,6 +70,12 @@ app.patch("/comments/:id", (req, res) => {
     const newComment = req.body.desc;
     const foundComment = comments.find(c => c.id === id);
     foundComment.desc = newComment;
+    res.redirect("/comments");
+})
+
+app.delete("/comments/:id", (req, res) => {
+    const {id} = req.params;
+    comments = comments.filter(c => c.id !== id);
     res.redirect("/comments");
 })
 
